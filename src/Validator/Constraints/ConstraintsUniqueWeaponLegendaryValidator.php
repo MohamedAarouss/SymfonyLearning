98e3a4d96dfa6e59bb965eeb36e410c5a0ae38e4
zimzim62000 <zimzim62000@gmail.com>
2020-11-15 23:11:36 +0100

    en cours validation

---
+++ src/Validator/Constraints/ConstraintsUniqueWeaponLegendaryValidator.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Validator\Constraints;
+
+use App\Entity\GameUser;
+use App\Entity\User;
+use App\Entity\Weapon;
+use App\Repository\GameUserRepository;
+use App\Repository\WeaponRepository;
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+use Symfony\Component\Validator\Exception\UnexpectedTypeException;
+use Symfony\Component\Validator\Exception\UnexpectedValueException;
+
+class ConstraintsUniqueWeaponLegendaryValidator extends ConstraintValidator
+{
+    private $weaponRepository;
+
+    public function __construct(WeaponRepository $weaponRepository)
+    {
+        $this->weaponRepository = $weaponRepository;
+    }
+
+    public function validate($weapon, Constraint $constraint)
+    {
+        if(!$constraint instanceof ConstraintsUniqueWeaponLegendary){
+            throw new UnexpectedTypeException($constraint, ConstraintsUniqueWeaponLegendary::class);
+        }
+
+        if($weapon->getUser() instanceof User && $weapon->getScarcity() === 8){
+            $exist = $this->weaponRepository->findOneBy(['User' => $weapon->getUser(), 'scarcity' => 8]);
+
+            if($exist instanceof Weapon){
+                if($weapon->getId() !== $exist->getId()){
+                    $this->context->buildViolation($constraint->message)
+                        ->setParameter('{{ username }}', $exist->getUser()->getUsername())
+                        ->addViolation();
+                }
+            }
+        }
+    }
+
+}
