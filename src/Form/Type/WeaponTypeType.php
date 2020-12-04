8be5f155a95fff9551690af37b6e4640bd75068a
zimzim62000 <zimzim62000@gmail.com>
2020-12-04 16:34:05 +0100

    fix type

--- src/Form/Type/WeaponTypeType.php
+++ src/Form/Type/WeaponTypeType.php
@@ -22,7 +22,7 @@ class WeaponTypeType extends AbstractType
                     ->orderBy('wp.name', 'DESC');
             },
             'choice_label' => 'name',
-            'compound' => true,
+            'compound' => false,
             'multiple' => false,
         ]);
     }
