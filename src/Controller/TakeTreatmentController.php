202395d923c289d3675655ec051d517169bea345
mohamed_aarouss <mohamed_aarouss@ens.univ-artois.fr>
2021-02-19 13:36:13 +0100

    DS - Question 3

--- src/Controller/TakeTreatmentController.php
+++ src/Controller/TakeTreatmentController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\TakeTreatment;
 use App\Form\TakeTreatmentType;
 use App\Repository\TakeTreatmentRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,6 +13,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 /**
  * @Route("/take/treatment")
+ * @IsGranted("ROLE_USER")
  */
 class TakeTreatmentController extends AbstractController
 {
